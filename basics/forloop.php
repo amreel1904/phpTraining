581235de4c6a1ec6ad4a0339e876872b771ad9a4
Amreel Nuqman <amreelnuqman.iman@gmail.com>
2023-09-13 00:51:45 +0800

    update

---
+++ basics/forloop.php
@@ -0,0 +1,9 @@
+<?php
+
+$outer = "I am Amreel ";
+
+for ($i = 0; $i < 6; $i++) {
+    $inner = "inside loop\n";
+    echo $outer;
+    echo $inner;
+}
--- basics/method.php
+++ basics/method.php
@@ -1,5 +1,16 @@
 <?php
-
-i(){
-    echo "Statement 1";
+function i1() {
+    echo "I dnl g eggs and ham\n";
 }
+
+function i2() {
+    echo "I dnl them, Sam I am!\n";
+}
+
+i1();
+i2();
+echo "I dnl them on boat\n";
+echo "I dnl the w goat\n";
+i2();
+i1();
+?>
